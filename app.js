e0d9c1b1de27d40d2b205ed811d2d1c45b443d07
diegorom27 <diegorom27@gmail.com>
2024-07-09 15:07:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send("Welcome to IBM Cloud DevOps with Tekton. Let's go use the Continuous Delivery Service test CI 3");
+  res.send("Welcome to IBM Cloud DevOps with Tekton. Let's go use the Continuous Delivery Service test CI 4");
 });
 
 app.listen(PORT);
